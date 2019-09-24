0bcd19458edb2e9a41cd1c42f419c46b6d62fc0e
Melvin Gault <melvin.gault@grenoble-inp.org>
2019-09-24 15:04:45 +0200

    ex 3 part 2

--- models.ts
+++ models.ts
@@ -1,4 +1,8 @@
 
 // In branch 'fix/typo': rename the type to Student
 // In branch 'feature/formatting': reformat the type on multiple lines for better readibility
-type Teacher = { name: string; year: '1A' | '2A' | '3A'; email: string }
+type Teacher = { 
+	name: string; 
+	year: '1A' | '2A' | '3A'; 
+	email: string
+}
